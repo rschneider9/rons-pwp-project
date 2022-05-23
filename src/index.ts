9c1744206f6c0e0e96c12af0530de3821acb0175
Ron Schneider <rschneider9@cnm.edu>
2022-05-23 15:04:48 -0600

    save point for testing

---
+++ src/index.ts
@@ -0,0 +1,88 @@
+import {Application} from 'express'
+import {Request, Response} from 'express'
+
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const Recaptcha = require('express-recaptcha').RecaptchaV2
+const formData = require('form-data')
+const Mailgun = require('mailgun.js')
+const mailgun = new Mailgun(formData)
+const {check, validationResult} = require('express-validator')
+require('dotenv').config()
+
+const validation = [
+    check('name', 'A valid name is required.').not().isEmpty().trim().escape(),
+    check('lastname', 'Please provide a valid last name').not().isEmpty().trim().escape(),
+    check('email', 'Please provide a valid Email.').isEmail,
+    check('phoneNumber', 'Please provide a valid phone number').trim().escape(),
+    check('comment', 'A message of up to 2000 characters is required.').trim().escape().isLength({min: 1, max:2000})
+
+]
+const app: Application = express()
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+const  recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
+
+const mailgunClient = mailgun.client ({username: 'api', key: process.env.MAILGUN_API_KEY})
+
+const handleGetRequest = (request: Request, response: Response) => {
+    return  response.json('This thing is on!')
+}
+
+const handlePostRequest = (request: Request, response: Response) => {
+    console.log('line36')
+
+    response.append('Content-Type', 'text/html')
+    response.append('Access-Control-Allow-Origin', '*')
+    return response.send('<div>It worked</div>')
+    // @ts-ignore
+    if(request.recaptcha.error) {
+        return response.send(
+            `<div class='alert alert-danger' role='alert'><strong>Oh Snap!</strong> There was a Recaptcha error. Please try again.</div>`
+        )
+    }
+
+    const errors = validationResult(request)
+
+    if (errors.isEmpty() === false) {
+         const currentError = errors.array()[0]
+        return response.send(
+            `<div class= 'alert alert-danger' role='alert'><strong>Oh Snaps!</strong> ${currentError.msg}</div>`
+        )
+    }
+    const {name, email, comment, phoneNumber, lastName} = request.body
+
+    const mailgunData ={
+        to: process.env.MAIL_RECIPIENT,
+        from: `${name}: ${lastName} <postmaster@${process.env.MAILGUN_DOMAIN}`,
+        subject: `${email}: ${phoneNumber}`,
+        text: comment
+    }
+
+    mailgunClient.messages.create(process.env.MAILGUN_DOMAIN, mailgunData)
+        .then((msg: any) =>
+            response.send(
+                `<div class='alert alert-success' role='alert'>Email Successfully Sent</div>`
+            ))
+        .catch((error: any) =>
+            response.send(
+                `<div class='alert alert-danger' role='alert'><strong>Oh Snap3!</strong> Email Failed. Please Try Again.</div>`
+            ))
+
+}
+
+const indexRoute = express.Router()
+
+indexRoute.route('/')
+    .get(handleGetRequest)
+    .post(recaptcha.middleware.verify, validation, handleGetRequest)
+
+app.use('/apis', indexRoute)
+
+app.listen(4200, () => {
+    console.log('Express Successfully Built')
+})
